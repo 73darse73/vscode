0232383e31f962d87f33b50c7ff1d9f839d1116c
73darse73 <73darse73@gmail.com>
2025-02-20 21:32:01 +0900

    グローバルなステートについてやった

--- udemy/react/udemy_react_state/src/components/pages/UserList.jsx
+++ udemy/react/udemy_react_state/src/components/pages/UserList.jsx
@@ -1,10 +1,18 @@
 import { HeaderOnly } from "../templates/HeaderOnly"
 import { CardList } from "../organism/CardList"
+import { SecondaryButton } from "../atoms/button/SecondaryButton"
+import { useContext } from "react"
+import { UserContext } from "../../providers/UserContext"
 
 export const UserList = () => {
+    const { userInfo, setUserInfo } = useContext(UserContext);
+    const onClickSwitch = () => {
+        setUserInfo({ isAdmin: !userInfo.isAdmin });
+    }
     return(
         <HeaderOnly>
             <h1>UserList</h1>
+            <SecondaryButton onClick={onClickSwitch}>切り替え</SecondaryButton>
             <CardList></CardList>
         </HeaderOnly>
     )
